2f06cd6992c0ab0f52ae301c56341eb3163a3c0c
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-03-04 17:01:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let configStr = '', tempStr = '';
+  bracketsConfig.forEach(element => {
+      configStr += element.join('');
+  });
+  for (let i = 0; i < str.length; i++) {
+      if (configStr.indexOf(str[i]) % 2 == 0) {
+          if (tempStr[tempStr.length-1] == str[i] && configStr[configStr.indexOf(str[i])] == configStr[configStr.indexOf(str[i])+1]) {
+              tempStr = tempStr.slice(0, -1);
+              console.log(tempStr);
+          }
+          else {
+              tempStr += str[i];
+              console.log(tempStr);
+          }
+      }
+      else {
+          if (configStr.indexOf(tempStr[tempStr.length-1]) == configStr.indexOf(str[i])-1) {
+              tempStr = tempStr.slice(0, -1);
+              console.log(tempStr);
+          }
+          else {
+              return false;
+          }
+      }
+  }
+  return tempStr.length > 0 ? false: true;
 }
