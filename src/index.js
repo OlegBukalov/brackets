5167b5d4f9fff65d4af6b70ee06162d417fcc228
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-03-04 17:14:26 +0200

    delete console.log()

--- src/index.js
+++ src/index.js
@@ -7,17 +7,14 @@ module.exports = function check(str, bracketsConfig) {
       if (configStr.indexOf(str[i]) % 2 == 0) {
           if (tempStr[tempStr.length-1] == str[i] && configStr[configStr.indexOf(str[i])] == configStr[configStr.indexOf(str[i])+1]) {
               tempStr = tempStr.slice(0, -1);
-              console.log(tempStr);
           }
           else {
               tempStr += str[i];
-              console.log(tempStr);
           }
       }
       else {
           if (configStr.indexOf(tempStr[tempStr.length-1]) == configStr.indexOf(str[i])-1) {
               tempStr = tempStr.slice(0, -1);
-              console.log(tempStr);
           }
           else {
               return false;
